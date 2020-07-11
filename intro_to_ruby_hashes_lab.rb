1ca34ec0f9612ce24d98e91dcf121fc2c138a507
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-11 16:41:42 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,16 @@
 def base_hash
+  monopoly = {:railroads => {}}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 
 end
 
 def monopoly_with_second_tier
+  monopoly = {:railroads => {}}
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly[:railroads][:names] = {}
+  monopoly
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -13,6 +19,21 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  monopoly = {:railroads => {}}
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly[:railroads][:names] = {}
+  
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  
+   monopoly[:railroads][:names][:reading_railroad] = {}
+   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+   monopoly[:railroads][:names][:b_and_o_railroad] = {}
+   monopoly[:railroads][:names][:shortline_railroad] = {}
+  monopoly
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -21,6 +42,28 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
+    monopoly = {:railroads => {}}
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly[:railroads][:names] = {}
+  
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  
+   monopoly[:railroads][:names][:reading_railroad] = {}
+   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+   monopoly[:railroads][:names][:b_and_o_railroad] = {}
+   monopoly[:railroads][:names][:shortline_railroad] = {}
+   railroads = monopoly[:railroads][:names]
+   railroads[:reading_railroad][:mortgage_value] = 100
+   railroads[:pennsylvania_railroad][:mortgage_value] = 200
+   railroads[:b_and_o_railroad][:mortgage_value] = 400
+   railroads[:shortline_railroad][:mortgage_value] = 800
+
+  monopoly
+ 
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
