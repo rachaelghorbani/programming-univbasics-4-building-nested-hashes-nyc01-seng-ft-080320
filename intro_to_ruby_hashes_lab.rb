6fad37b9324d517f8b3df46015ded44a520b5160
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-11 16:34:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,6 +30,7 @@ def monopoly_with_third_tier
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
   
    monopoly[:railroads][:names][:reading_railroad] = {}
+   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
  
   monopoly
   # When you start writing the implementation for this method, copy the Hash
